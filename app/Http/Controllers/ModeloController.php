007f5a03f99ff48420445581b6ae541479afbdba
momojuniior <momo.ju@hotmail.com>
2024-07-06 10:43:39 -0300

    feat: API Webservice implementation for Modelo

--- app/Http/Controllers/ModeloController.php
+++ app/Http/Controllers/ModeloController.php
@@ -2,19 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Storage;
 use App\Models\Modelo;
 use Illuminate\Http\Request;
 
 class ModeloController extends Controller
 {
+    public $modelo;
+    public function __construct(Modelo $modelo)
+    {
+        $this->modelo = $modelo;
+    }
+
     /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+        $modelos = array();
+
+        if ($request->has('atributos_marca')) {
+            $atributos_marca = $request->atributos_marca;
+            $modelos = $this->modelo->with('marca:id,' .$atributos_marca);
+        } else {
+            $modelos = $this->modelo->with('marca');
+        }
+
+        if($request->has('filtro')) {
+            $condicoes = explode(':', $request->filtro);
+            $modelos = $modelos->where($condicoes[0], $condicoes[1], $condicoes[2]);
+        }
+        
+        if($request->has('atributos')) {
+            $atributos = $request->atributos;
+            // dd($atributos_marca);
+            $modelos = $modelos->selectRaw($atributos)->with('marca:id,' .$atributos_marca)->get();
+        } else {
+            $modelos = $modelos->get();
+        }
+
+        // return response()->json($this->modelo->with('marca')->get(), 200);
+        return response()->json($modelos, 200);
+        //all() -> cria um objeto de consulta + get() = collection
+        //get() -> modifica a consulta -> collection
     }
 
     /**
@@ -35,7 +67,22 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate($this->modelo->rules());
+        
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens/modelos', 'public');
+
+        $modelo = $this->modelo->create([
+            'marca_id' => $request->marca_id,
+            'nome' => $request->nome,
+            'imagem' => $imagem_urn,
+            'numero_portas' => $request->numero_portas,
+            'lugares' => $request->lugares,
+            'air_bag' => $request->air_bag,
+            'abs' => $request->abs
+        ]);
+        
+        return response()->json($modelo, 201);
     }
 
     /**
@@ -44,9 +91,15 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+        $modelo = $this->modelo->with('marca')->find($id);
+
+        if($modelo === null) {
+            return response()->json(['erro' => 'Recurso pesquisado não existe.'], 404);
+        }
+
+        return response()->json($modelo, 200);
     }
 
     /**
@@ -67,9 +120,52 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        }
+
+        if($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+            
+             foreach ($modelo->rules() as $input => $regra) {
+                if(array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+             }
+            
+            $request->validate($regrasDinamicas);
+
+        } else {
+            $request->validate($modelo->rules());
+        }
+
+        // remove o arquivo antigo, caso um novo arquivo tenha sido enviado no request
+        if($request->file('imagem')) {
+            Storage::disk('public')->delete($modelo->imagem);
+        }
+
+        $image = $request->file('imagem');
+        $imagem_urn = $image->store('imagens/modelos', 'public');
+
+        $modelo->fill($request->all());
+        $modelo->imagem = $imagem_urn;
+        $modelo->save();
+
+        // $modelo->update([
+        //     'marca_id' => $request->marca_id,
+        //     'nome' => $request->nome,
+        //     'imagem' => $imagem_urn,
+        //     'numero_portas' => $request->numero_portas,
+        //     'lugares' => $request->lugares,
+        //     'air_bag' => $request->air_bag,
+        //     'abs' => $request->abs
+        // ]);
+
+        return response()->json($modelo, 200);
     }
 
     /**
@@ -78,8 +174,19 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+        $modelo = $this->modelo->find($id);
+
+        if($modelo === null) {
+            return response()->json(['erro' => 'Impossível realizar a atualização. O recurso solicitado não existe'], 404);
+        }
+
+        // remove o arquivo antigo
+        Storage::disk('public')->delete($modelo->imagem);
+        
+
+        $modelo->delete();
+        return response()->json(['msg' => 'O modelo foi removido com sucesso!'], 200);
     }
 }
